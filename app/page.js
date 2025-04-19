11e725593e12330468460d4eca483c65f78494fe
corncob <ngai_to_lo@hotmail.com>
2025-04-19 15:00:23 -0700

    removed deprecated code from pages. need to clean up the db api

--- app/page.js
+++ app/page.js
@@ -2,32 +2,11 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [startDate, setStartDate] = useState(null);
   const [daysSince, setDaysSince] = useState(0);
   const [trucks,setTrucks] = useState([]);
   const [link, setLink] = useState('');
   const [datelink, setDatelink] = useState('')
-  const [links, setLinks] = useState([]);
   const [error,setError] = useState(null);
-
-  // useEffect(() => {
-  //   const savedDate = localStorage.getItem('startDate');
-  //   const savedLinks = JSON.parse(localStorage.getItem('links')) || [];
-
-  //   if (savedDate) {
-  //     setStartDate(new Date(savedDate));
-  //   }
-  //   setLinks(savedLinks);
-  // }, []);
-//this should be deprecated 
-
-  useEffect(() => {
-    if (startDate) {
-      const now = new Date();
-      const diffTime = Math.floor((now - new Date(startDate)) / (1000 * 60 * 60 * 24));
-      setDaysSince(diffTime);
-    }
-  }, [startDate]);
 
   //calls the database api takes the dates  and linksto the articles and maps to arrays each.
   useEffect(() => {
@@ -48,7 +27,7 @@ export default function Home() {
     // console.log(`this is 1 ${data.users[0]}`);
 
     //creates an object; temp  that maps dates and links to
-    setStartDate(  new Date( data.users[0].date))
+
     var temp  = datesArr.map((date,index)=>({
       datestr: formatDateTo(date),
       linkto : linksArr[index]
@@ -100,7 +79,7 @@ export default function Home() {
       });
 
       if ( response.ok) {
-         await fetchData();  // 🔥 Re-fetch data immediately after adding new entry
+         await fetchData();  // Re-fetch data immediately after adding new entry
          setLink('');
          setDatelink('')
       } else {
@@ -173,35 +152,3 @@ export default function Home() {
     </div>
   );
 }
-
-
- // old way with anarray where i destructure it with local data
-  
-  
-
-  // const handleReset = () => {
-  //   if (!link) return;
-  //   const now = new Date();
-  //   setStartDate(now);
-  //   setDaysSince(0);
-  //   const newLinks = [link, ...links];
-  //   setLinks(newLinks);
-  //   localStorage.setItem('startDate', now.toISOString());
-  //   localStorage.setItem('links', JSON.stringify(newLinks));
-  //   setLink('');
-  // };
-
-
-  //old method where trucks was one simple item 
-  // {/* {trucks.length > 0 && (
-  //         <div className="mt-8">
-  //           <h2 className="text-lg font-semibold">Recent Links:</h2>
-  //           <ul className="mt-4 space-y-2">
-  //             {trucks.map((l, index) => (
-  //               <li key={index} className="text-blue-500 underline">
-  //                 <a href={l} target="_blank" rel="noopener noreferrer">{l}</a>
-  //               </li>
-  //             ))}
-  //           </ul>
-  //         </div>
-  //       )} */}
